bd50a95e27923a790c4c65630605c8f8ccdc89ee
erfaneh.srchl <erfaneh.s7713@gmail.com>
2026-06-12 17:56:28 +0330

    InitialCommit

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using Domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddDomain();
+
+        return services;
+    }
+}
